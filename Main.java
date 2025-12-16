3e2b1820e7a95207b034ba4bf825e0b4d3298d9f
Vitaliy <vp080607@mail.ru>
2025-12-16 14:08:18 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,16 +8,33 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         out.println("Выберите опцию:" + "\n" + "1 - начать новую игру" + "\n" + "0 - не начинать новую игру");
-        int f = in.nextInt();
-        String none = in.nextLine();
-        while (f == 1) {
+        String f = in.nextLine();
+        while (!f.equals("1") && !f.equals("0")){
+            out.println("Ошибка ввода, такой функции не существует");
+            out.println("Выберите опцию:" + "\n" + "1 - начать новую игру" + "\n" + "0 - не начинать новую игру");
+            f = in.nextLine();
+        }
+        while (f.equals("1")) {
             out.println("Введите имена игроков через enter (состоящие из одной буквы):");
             String n1 = in.nextLine();
             String n2 = in.nextLine();
-            while (n1.equals(n2)) {
-                out.println("Имена не могут быть одинаковыми, введите новые");
-                n1 = in.nextLine();
-                n2 = in.nextLine();
+            while (n1.length() != 1 || n2.length() != 1 || n1.equals(n2) || n1.equals("-") || n2.equals("-") || n1.equals(" ") || n2.equals(" ")) {
+                if (n1.length() != 1 || n2.length() != 1){
+                    out.println("Неверная длина имен(возможно поставлен лишний пробел)");
+                    n1 = in.nextLine();
+                    n2 = in.nextLine();
+                }
+                else
+                    if (n1.equals(n2)){
+                        out.println("Имена не должны быть одинаковыми");
+                        n1 = in.nextLine();
+                        n2 = in.nextLine();
+                    }
+                    else {
+                        out.println("Именна должны быть символом(за исключением - и проблела");
+                        n1 = in.nextLine();
+                        n2 = in.nextLine();
+                    }
             }
             Game pl = new Game(n1, n2);
             out.println(pl);
@@ -29,21 +46,18 @@ public class Main {
                     q = 10;
                     out.println("Победил игрок " + n1 + "!");
                     out.println("Выберите опцию:" + "\n" + "1 - начать новую игру" + "\n" + "0 - не начинать новую игру");
-                    f = in.nextInt();
-                    none = in.nextLine();
+                    f = in.nextLine();
                 } else if (pl.win(q) == 2) {
                     q = 10;
                     out.println("Победил игрок " + n2 + "!");
                     out.println("Выберите опцию:" + "\n" + "1 - начать новую игру" + "\n" + "0 - не начинать новую игру");
-                    f = in.nextInt();
-                    none = in.nextLine();
+                    f = in.nextLine();
                 } else {
                     q++;
                     if (pl.draw(q)){
                         out.println("Ходы кончились, никто не выиграл, ничья!");
                         out.println("Выберите опцию:" + "\n" + "1 - начать новую игру" + "\n" + "0 - не начинать новую игру");
-                        f = in.nextInt();
-                        none = in.nextLine();
+                        f = in.nextLine();
                     }
                     else
                         out.println("Игра в процессе");
@@ -66,13 +80,6 @@ class Game{
         this.Name1 = Name1;
         this.Name2 = Name2;
     }
-    public void newgame(){
-        for (int i = 0; i<3; i++){
-            for (int j = 0; j<3; j++){
-                pole[i][j] = "-";
-            }
-        }
-    }
     public String cheyxod(int q){
         if (q%2==0){
             return "Ход игрока " + Name2;
@@ -86,27 +93,48 @@ class Game{
     public void xod(int q) {
             System.out.println();
             System.out.println(cheyxod(q));
-            System.out.println("Введите координаты через пробел");
-            int x = in.nextInt();
-            int y = in.nextInt();
+            System.out.println("Введите координаты через enter(Без лишних пробелов)");
+            String x1 = in.nextLine();
+            String y1 = in.nextLine();
+            while (x1.length() != 1 || y1.length() != 1 || !('0' < x1.charAt(0) && x1.charAt(0) < '4' && '0' < y1.charAt(0) && y1.charAt(0) < '4')){
+                System.out.println("Ошибка ввода(проверьте на наличие лишних пробелов)");
+                x1 = in.nextLine();
+                y1 = in.nextLine();
+            }
+            int x = x1.charAt(0) - '0';
+            int y = y1.charAt(0) - '0';
             while (!(0 < x && x < 4 && 0 < y && y < 4) || !pole[x - 1][y - 1].equals("-")) {
                 if (!(0 < x && x < 4 && 0 < y && y < 4)) {
                     System.out.println();
                     System.out.println("Введены неверные координаты!");
                     System.out.println(cheyxod(q));
-                    System.out.println("Введите координаты через пробел");
-                    x = in.nextInt();
-                    y = in.nextInt();
+                    System.out.println("Введите координаты через enter(Без лишних пробелов)");
+                    x1 = in.nextLine();
+                    y1 = in.nextLine();
+                    while (x1.length() != 1 || y1.length() != 1 || !('0' < x1.charAt(0) && x1.charAt(0) < '4' && '0' < y1.charAt(0) && y1.charAt(0) < '4')) {
+                        System.out.println("Ошибка ввода(проверьте на наличие лишних пробелов)");
+                        x1 = in.nextLine();
+                        y1 = in.nextLine();
+                    }
+                    x = x1.charAt(0) - '0';
+                    y = y1.charAt(0) - '0';
                 }
                 else{
                     System.out.println();
                     System.out.println("Это поле занято, введи другие координаты!");
                     System.out.println(cheyxod(q));
-                    System.out.println("Введите координаты через пробел");
-                    x = in.nextInt();
-                    y = in.nextInt();
+                    System.out.println("Введите координаты через enter(Без лишних пробелов)");
+                    x1 = in.nextLine();
+                    y1 = in.nextLine();
+                    while (x1.length() != 1 || y1.length() != 1 || !('0' < x1.charAt(0) && x1.charAt(0) < '4' && '0' < y1.charAt(0) && y1.charAt(0) < '4')) {
+                        System.out.println("Ошибка ввода(проверьте на наличие лишних пробелов)");
+                        x1 = in.nextLine();
+                        y1 = in.nextLine();
+                    }
+                    x = x1.charAt(0) - '0';
+                    y = y1.charAt(0)  - '0';
+                    }
                 }
-            }
             if (q % 2 == 0) {
                 pole[x - 1][y - 1] = this.Name2;
             } else {
